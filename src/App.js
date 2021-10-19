e4aee7582d1c90bb2d499ea8ddcd56a8ab3bcfd9
orwa97 <orwa.aloufi@gmail.com>
2021-10-19 23:02:32 +0300

    create the button component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { ReactComponent as Settings } from "./images/SVG/cog.svg";
 import Button from "./components/button/Button";
 import Select from "./components/Select/Select";
 
@@ -6,7 +7,9 @@ function App() {
   return (
     <Fragment>
       <Select postfix="post" options={["a", "b", "c", "d"]} />
-      <Button />
+      <Button icon={<Settings />} type="icon-text">
+        button
+      </Button>
     </Fragment>
   );
 }
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,5 +1,23 @@
+import "./Button.scss";
+import classNames from "classnames";
 const Button = (props) => {
-  return <button>{props.children}</button>;
+  return (
+    <button
+      className={classNames(
+        "btn",
+        { "btn--icon-only": props.type === "icon-only" },
+        { "btn--text-only": props.type === "text-only" },
+        { "btn--textNicon": props.type === "icon-text" }
+      )}
+    >
+      {props.type != "text-only" && <div className="logo">{props.icon}</div>}
+      {props.children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: "text-only",
 };
 
 export default Button;
