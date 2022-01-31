ea8370c9a644462c12ba8a676655b6a460969802
orwa97 <orwa.aloufi@gmail.com>
2022-01-31 23:33:58 +0200

    addin theme01 functionality (updating atoms) + fixing some bugs

--- src/pages/Main/muqtatif/muqHeader/themesDropDown/ThemesDropDown.jsx
+++ src/pages/Main/muqtatif/muqHeader/themesDropDown/ThemesDropDown.jsx
@@ -3,12 +3,41 @@ import classes from "./ThemesDropDown.module.scss";
 import theme_01 from "../../../../../images/themes/theme_01.png";
 import { useAtom } from "jotai";
 import { SelectedThemeAtom } from "./ThemesAtoms";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { useUpdateAtom } from "jotai/utils";
+import { textBgAtom } from "../settings/window/WindowAtoms";
+import {
+  fontColorAtom,
+  fontSizeAtom,
+  textAlignAtom,
+} from "../settings/editor/EditorAtoms";
+import { useEffect } from "react";
+import { useCallback } from "react";
 const ThemesDropDown = (props) => {
+  const setTextBgIsChecked = useUpdateAtom(textBgAtom);
+  const setFontColor = useUpdateAtom(fontColorAtom);
+  const setTextAlign = useUpdateAtom(textAlignAtom);
+  const setFontSize = useUpdateAtom(fontSizeAtom);
+  const [themeIsSelected, setThemeIsSelected] = useState(false);
   const [selectedTheme, setSelectedTheme] = useAtom(SelectedThemeAtom);
-  const themesHandler = (e) => {
+  const themesHandler = useCallback((e) => {
     setSelectedTheme(e.target.id);
-  };
+  }, []);
+  const themesAltHandler = useCallback((e) => {
+    setThemeIsSelected(true);
+  }, []);
+  // update Window's & Editor's states based on the selected theme
+  useEffect(() => {
+    const themeNum = !!selectedTheme ? selectedTheme.split("-")[2] : null;
+    if (themeIsSelected) {
+      if (themeNum === "01") {
+        setTextBgIsChecked(false);
+        setFontColor("#c3845b");
+        setTextAlign("center");
+        setFontSize("2.5");
+      }
+    }
+  }, [selectedTheme, themeIsSelected]);
 
   const radioBtnLabels = useMemo(
     () => [
@@ -34,6 +63,7 @@ const ThemesDropDown = (props) => {
         flexDirection="row"
         onChange={themesHandler}
         isSelected={selectedTheme}
+        onClick={themesAltHandler}
       />
     </div>
   );
